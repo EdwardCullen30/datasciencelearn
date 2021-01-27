a7f422150420c1f465197c9b481ebc21162691a7
EdwardCullen30 <f20180872@pilani.bits-pilani.ac.in>
2021-01-28 00:42:06 +0530

    start

---
+++ Prog.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import quandl
+import math
+
+df = quandl.get("WIKI/GOOGL")
+df.columns = df.columns.to_series().apply(lambda x: x.strip())
+df = df[['Adj. Open', "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume", ]]
+df['HL_PCT'] = (df["Adj. High"]-df["Adj. Close"]) / df["Adj. Close"]*100
+df['PCT_chag'] = (df["Adj. Close"]-df["Adj. Open"]) / df["Adj. Close"]*100
+df = df[["Adj. Close", "HL_PCT", "PCT_chag"]]
+forcast_col = "Adj. Close"
+df.fillna(-99999, inplace=True)
+forcast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forcast_col].shift(-forcast_out)
+df.dropna(inplace=True)
+print(df.head())
